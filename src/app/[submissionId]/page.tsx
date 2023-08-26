f1de474d28b7f181a909c6527a83fd9851450988
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2023-08-26 13:59:50 +0200

    Missed one dvh

--- src/app/[submissionId]/page.tsx
+++ src/app/[submissionId]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({params: { submissionId }}: {params: { submis
     }) ?? notFound()
 
     return (
-        <main className={"flex min-h-screen flex-col items-center gap-6 sm:gap-12 p-6 sm:p-12"}>
+        <main className={"flex min-h-screen-dvh flex-col items-center gap-6 sm:gap-12 p-6 sm:p-12"}>
 
             <div className={"flex items-center"}>
                 <Link href={"/"}><Button variant={"link"}><ArrowLeft/></Button></Link>
